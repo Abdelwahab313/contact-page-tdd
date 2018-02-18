7ff92d53d710666addcdd1b70d9e246e1e578def
Abdelwahab313 <abdelwahabahmed93@gmail.com>
2018-02-18 18:23:06 +0200

    Test for validating the username

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,25 +1,44 @@
 import React, {component}  from 'react';
 import './contact.css';
-import Form from 'react-validation/build/form';
-import Input from 'react-validation/build/input';
-
-
 
 export default class ContactForm extends React.Component {
+	constructor(props){
+		super(props)
+		this.state ={
+			values:{
+				username:"",
+			},
+			isDisabled:true,
+
+		};
+		this.handleUsernameChange = this.handleUsernameChange.bind(this)
+	}
+	handleUsernameChange(evt){
+		if(!evt.target.value){
+			this.setState({isDisabled:true})
+			// console.log(this.state.isDisabled)
+		}
+		else{
+			// console.log('sdf')
+			this.setState({isDisabled:false})
+
+		}
+		this.setState({values:{username:evt.target.value}})
+	}
 	render() {
 		return (
 				<form>
 					<h2>Contact us</h2>
-					<input type="text" id="fname" value="" name="fname" placeholder="Your name.."/>
+					<input type="text"  id="fname" value={this.state.values.username} onChange={this.handleUsernameChange} name="fname" placeholder="Your name.." />
 
-   					<input type="email" id="lname" name="email" placeholder="Your email.."/>
+   					<input type="email" id="lname" name="email" placeholder="Your email.." />
 
    					<textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-
-    				<input type="submit" value="Submit" disabled/>
+   					<span id="errorSp" style={{display:"block"}}>{this.state.isDisabled?"error":"0	"}</span>
+    				<input type="submit" value="Submit" disabled={this.state.isDisabled}/>
 				</form>
 
-    				)
+    			)
 
 	}
 }
